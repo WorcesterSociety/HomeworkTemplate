b2bf1bdcc6e89749e43c37213ed4c22914c75eee
Aaron Weiss <aaronweiss74@gmail.com>
2017-02-19 03:30:09 -0500

    Fixed init.sh.

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@ if [ "$NAME" == '' ]
 then
     echo "init.sh: expects 1 argument -- the desired name for the homework document."
 else
-    sed s/example/$NAME/g Makefile > Makefile
+    sed s/example/$NAME/g Makefile > Makefile.new
+    mv Makefile.new Makefile
+    mv example.tex $NAME.tex
     rm -rf .git LICENSE README.md
     rm init.sh
 fi
